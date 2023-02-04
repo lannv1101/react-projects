3a22aa2c568b850adc831bcdb3e070dd53af4ada
nguyenhuynhnhu <bienthinhu61@gmail.com>
2023-02-04 12:26:26 +0700

    update tours

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, name, info, price, removeTour }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
       <footer>
         <div className="tour-info">
-          <h3>{name}</h3>
+          <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
         <p>
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
-            {readMore ? "show less" : "show more"}
+            {readMore ? 'show less' : ' read more'}
           </button>
         </p>
         <button className="delete-btn" onClick={() => removeTour(id)}>Not interested</button>
